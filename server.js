4686140749f8005f1739433e909889e1621ba298
nathank <nathank@btcx.com>
2022-03-30 22:45:04 -0700

    Use event ids to compare runs

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ const BASEURL = "https://maker.ifttt.com/trigger/";
 const WITHKEY = "/with/key/";
 const DEFAULT_DELAY_MINS = process.env.DEFAULT_DELAY_MINUTES
 
-var actions_requests = {}
-var count = 0
+var actionsEventIds = {}
+var eventId = 0
 
 // Handle requests from IFTTT
 app.post("/", function (request, response) {
-  let this_event_count = count
-  count += 1
+  // Use eventId to see if action should be run
+  let thisEventId = eventId
+  eventId += 1
   
   console.log("Request received from IFTTT");
   console.log(request.body);
@@ -43,54 +44,37 @@ app.post("/", function (request, response) {
   let delayMs = delayMinutes * 60 * 1000
   console.log(`From JSON, delayMinutes is ${delayMinutes}`)
   
-  
-  // Handle old request, if exists
-  let old_request = actions_requests[action]
-  // console.log(`this is old_request for count ${this_event_count}:`)
-  // console.log(old_request)
-  // console.log(`end old_request for count ${this_event_count} \n`)
-  
-  if (old_request != null) {
-    console.log("destroying old_request")
-    // Handle old request
-    old_request.abort()
-    // old_request.destroy()
-    // old_request.shouldKeepAlive = false
-    
-    // Put this request in its place
-    actions_requests[action] = request
-  } else {
-    // Have not seen this action before
-    actions_requests[action] = request
-  }
-  
-  // TODO handle multiple actions using hashmap
-  
-  // TODO seperate timer for each action
-  // TODO reset timer if action is seen before trigger time, https://stackoverflow.com/questions/315078/how-do-you-handle-multiple-instances-of-settimeout
+  // Store the most up-to-date event id for that action
+  actionsEventIds[action] = eventId
   
   // Log when it will execute
   let executeDate = moment().tz('America/Los_Angeles').add(delayMinutes, 'm').format("YYYY-MM-DD h:mm:ss a")
-  console.log(`Executing ${action} in the future at: ${executeDate}`);
+  console.log(`Executing ${action} with eventId ${thisEventId} in the future at: ${executeDate}`);
   
   // Set timeout and then execute after timeout
   setTimeout(() => {
-    makeRequest(action, this_event_count)
+    makeRequest(action, thisEventId)
   }, delayMs);
   
-  console.log(`Trigger set for count ${this_event_count}`);
+  console.log(`Trigger set for eventId ${thisEventId}`);
   response.end();
 });
 
 
 
-function makeRequest(action, this_event_count) {
-  console.log(`Making request with action ${action} for count ${this_event_count}`)
-  request(BASEURL + action + WITHKEY + IFTTT_ID, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log(`${body} with count ${this_event_count}`); // Show the response from IFTTT
-    } else {
-      console.log(BASEURL + action + WITHKEY + "MY_KEY" + ": " + error); // Show the error
-    }
-  });
+function makeRequest(action, thisEventId) {
+  let mostRecentEventId = actionsEventIds[action]
+  if (mostRecentEventId === thisEventId) {
+    console.log(`Making request with action ${action} for eventId ${thisEventId}`)
+    request(BASEURL + action + WITHKEY + IFTTT_ID, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(`${body} with eventId ${thisEventId}`); // Show the response from IFTTT
+      } else {
+        console.log(BASEURL + action + WITHKEY + "MY_KEY" + ": " + error); // Show the error
+      }
+    });
+  } else {
+    console.log(`Found a more recent event. Was looking for ${thisEventId} and found ${actionsEventIds[action]}`)
+  }
+  
 }
