b6c6e8d5ea043ce5b82cfa9a8b6b15296e599ab0
nathank <2602602+nathankwok@users.noreply.github.com>
2023-01-06 01:08:12 -0800

    Remove need for to_remove tag

--- workflow_scripts/remove_images_using_tags.sh
+++ workflow_scripts/remove_images_using_tags.sh
@@ -1,19 +1,9 @@
 # Forked from https://gist.github.com/ferferga/93ca1ab3056257d05f6e33af0d6ead49
 
 container="${1}"
-tag="${2}"  # Not giving a 2nd arg or giving "" will result in deleting untagged (dangling) images
+echo "Fetching ${container} dangling images from GHCR"
 
-if [ "${tag}" = "" ]
-then
-  tag_echo_name=dangling
-else
-  tag_echo_name="${container}"
-fi
-
-echo "Fetching ${container} images with ${tag_echo_name} tag from GHCR"
-
-
-filter=".[] | select(.metadata.container.tags==[\"${tag}\"]) | .id"
+filter=".[] | select(.metadata.container.tags==[]) | .id"
 ids_to_delete=$(gh api /user/packages/container/${container}/versions | jq -r "${filter}")
 
 echo "Found image ids:"
@@ -25,7 +15,7 @@ then
 	exit 0
 fi
 
-echo -e "\nDeleting ${container} images with ${tag_echo_name} tag..."
+echo -e "\nDeleting dangling images..."
 while read -r line; do
 	id="$line"
 	## Workaround for https://github.com/cli/cli/issues/4286 and https://github.com/cli/cli/issues/3937
@@ -33,5 +23,5 @@ while read -r line; do
 	echo Dangling image with ID $id deleted successfully
 done <<< $ids_to_delete
 
-echo -e "\nAll the ${container} images with ${tag_echo_name} tag have been removed successfully"
+echo -e "\nAll dangling images have been removed successfully"
 exit 0
